a572186e28465054bf9aa124755f6a269ac6ceff
Onlee <Onlee@gmail.com>
2021-09-08 21:33:06 +0800

    共享RequestScope对象数据

---
+++ SpringMVC/src/main/java/com/springmvc/study/controller/ScopeController.java
@@ -0,0 +1,62 @@
+package com.springmvc.study.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
+@Controller
+public class ScopeController {
+
+    // 使用HttpServletRequest共享数据
+    @RequestMapping("/testRequestByServletAPI")
+    public String testRequestByServletAPI(HttpServletRequest request) {
+        request.setAttribute("testRequestScope", "hello,servletAPI");
+        return "success";
+    }
+
+    // 使用ModelAndView共享数据
+    @RequestMapping("/testModelAndView")
+    public ModelAndView testModelAndView() {
+        /**
+         * ModelAndView有Model和View的功能
+         * Model 主要向请求域共享数据
+         * View 主要用于设置试图，实现页面跳转
+         */
+        ModelAndView mav = new ModelAndView();
+        // 处理模型数据，即向请求域request共享数据
+        mav.addObject("testRequestScope", "hello,ModelAndView");
+        // 设置试图名称
+        mav.setViewName("success");
+
+        return mav;
+    }
+
+    // 使用Model共享数据
+    @RequestMapping("/testModel")
+    public String testModel(Model model) {
+        model.addAttribute("testRequestScope", "hello,model");
+        System.out.println(model.getClass().getName());
+        return "success";
+    }
+
+    // 使用Map共享数据
+    @RequestMapping("/testMap")
+    public String testMap(Map<String, Object> map) {
+        map.put("testRequestScope", "hello,map");
+        System.out.println(map);
+        return "success";
+    }
+
+    // 使用ModelMap共享数据
+    @RequestMapping("/testModelMap")
+    public String testModelMap(ModelMap modelMap) {
+        modelMap.addAttribute("testRequestScope", "hello,ModelMap");
+        System.out.println(modelMap);
+        return "success";
+    }
+}
