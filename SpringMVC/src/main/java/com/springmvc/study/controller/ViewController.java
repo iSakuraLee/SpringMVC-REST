372778295638d69051dc84bd405cf9afe18c339e
Onlee <Onlee@gmail.com>
2021-09-21 21:33:31 +0800

    视图解析器

--- SpringMVC/src/main/java/com/springmvc/study/controller/ViewController.java
+++ SpringMVC/src/main/java/com/springmvc/study/controller/ViewController.java
@@ -16,4 +16,18 @@ public class ViewController {
         return "success";
     }
 
+    // 转发浏览器内部只发送了一次请求，地址栏出现的是testForward，可以获取请求域对象中的数据
+    // 不可以跨域
+    @RequestMapping("/testForward")
+    public String testForward() {
+        return "forward:/testThymeleafView";
+    }
+
+    // 重定向浏览器内部发送了两次请求：第一次访问Servlet；第二次访问重定向的地址，地址栏最终出现的地址是testThymeleafView
+    // 可以跨域，不能访问web-inf目录下的资源
+    @RequestMapping("/testRedirect")
+    public String testRedirect() {
+        return "redirect:/testThymeleafView";
+    }
+
 }
